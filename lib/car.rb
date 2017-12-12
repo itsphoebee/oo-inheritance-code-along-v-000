452cf7f45e75487b6746a9173d11c08c5922ce00
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 03:33:36 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,7 @@
 require_relative "./vehicle.rb" #needs access to the parent class
 
 class Car < Vehicle             # < inherit car from vehicle
-
+  def go
+   "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+ end
 end
